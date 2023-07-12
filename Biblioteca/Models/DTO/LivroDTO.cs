062edfd05a41f4680f9b3af06e79cf41662e4dfc
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-12 10:39:45 -0300

    Iniciando database fake

--- Biblioteca/Models/DTO/LivroDTO.cs
+++ Biblioteca/Models/DTO/LivroDTO.cs
@@ -8,18 +8,18 @@
         public string Autor { get; set; }
         public string Editora { get; set; }
         public DateTime DataPublicacao { get; set; }
-        public long ISBN { get; set; }
+        public string ISBN { get; set; }
 
         // Construtores
         public LivroDTO(
             string id, string nome, string autor, string editora, 
-            DateTime dataPublicacao, long isbn
+            DateTime dataPublicacao, string isbn
             ) : this(nome, autor, editora, dataPublicacao, isbn)
         {
             this.ID = id;
         }
         
-        public LivroDTO(string nome, string autor, string editora, DateTime dataPublicacao, long isbn)
+        public LivroDTO(string nome, string autor, string editora, DateTime dataPublicacao, string isbn)
         {
             this.Nome = nome;
             this.Editora = editora;
---
+++ Biblioteca/Models/Repositories/ContextDataFake.cs
@@ -0,0 +1,20 @@
+﻿using Biblioteca.Models.DTO;
+
+namespace Biblioteca.Models.Repositories
+{
+    public static class ContextDataFake
+    {
+        public static List<LivroDTO> Livros;
+
+        static ContextDataFake()
+        {
+            Livros = new List<LivroDTO>();
+            InitializeData();
+        }
+
+        private static void InitializeData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
