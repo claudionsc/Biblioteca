25ea94f7ee450a441cdf6403b4ee449498267550
alvigabriel <claudio.alves20112011@hotmail.com>
2023-07-13 10:51:37 -0300

    Criando classe abstrata entidade, para gerar um id automaticamente, herdando id da entidade na classe LivroDTO

---
+++ Biblioteca/Models/Entidades/EntidadeBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Biblioteca.Models.Entidades
+{
+    /* 
+        Um GUID é um número inteiro de 128 bits (16 bytes) que você pode usar em 
+        todos os computadores e redes sempre que um identificador exclusivo for 
+        necessário
+
+    =====================================================================================
+
+    Use o modificador abstract em uma declaração de classe para indicar que uma 
+    classe se destina somente a ser uma classe base de outras classes, não instanciada 
+    por conta própria
+
+    */
+    public abstract class EntidadeBase
+    {
+        public string ID { get; set; }
+
+        public EntidadeBase() 
+        {
+            // Gerar um identificador único
+            ID = Guid.NewGuid().ToString();
+        }
+    }
+}
